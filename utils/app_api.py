fc311b2458eaf8b5796cb4d93ee4e01c173982b6
NivekTar <67305531+NivekTar@users.noreply.github.com>
2023-06-01 16:40:36 -0500

    RC-14 Utilidades - uso en rdp

---
+++ utils/app_api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from gpt_scrap import Verificar
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the RealCheck API_LLM!'
+
+@app.route('/analizar_llm', methods=['POST'])
+def analizar():
+    texto = request.get_json()['texto']
+    fuente1 = request.get_json()['fuente1']
+    fuente2 = request.get_json()['fuente2']
+    return jsonify(Verificar(texto, fuente1, fuente2))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7000, debug=True, use_reloader=True) 
