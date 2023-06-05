a3e267082e27d35f65f598ba2466f6748bb08182
NivekTar <67305531+NivekTar@users.noreply.github.com>
2023-06-05 00:26:05 -0500

    Corrección manejo de ventanas

--- utils/gpt_scrap.py
+++ utils/gpt_scrap.py
@@ -25,13 +25,12 @@ argos_es_en = get_argos_model('Spanish', 'English')
 ventanas = driver.window_handles
 
 def obtener_respuesta(texto):
-    driver.switch_to.window(ventanas[1])
+    driver.switch_to.window(ventanas[0])
     time.sleep(2)
     # Obtener la URL actual
     current_url = driver.current_url
     print("URL: ",current_url)
     # Verificar si la URL es "https://bard.google.com/"
-    print("c: ", current_url)
     if current_url == "https://bard.google.com/?hl=en":
         # Encontrar el elemento de texto (textarea) mediante su identificador (id)
         text_area = driver.find_element(By.ID, "mat-input-0")
@@ -101,7 +100,7 @@ def separar_respuesta(texto):
 
 
 def analizar_con_llm(texto, fuente1, fuente2, ad_info):
-    driver.switch_to.window(ventanas[0])
+    driver.switch_to.window(ventanas[1])
     time.sleep(2)
     print("URL: ", driver.current_url)
     # buscar <div class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"><textarea id="prompt-textarea" tabindex="0" data-id="root" rows="1" placeholder="Send a message..." class="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0" style="max-height: 200px; height: 24px; overflow-y: hidden;"></textarea><button disabled="" class="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2 disabled:opacity-40"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg></button></div> dar click
